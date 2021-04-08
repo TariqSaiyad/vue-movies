fb3244dcd2bb6ed7f81d9481d44946e197c92f51
Tariq Saiyad <tariqsaiyad98@gmail.com>
2021-04-08 18:51:46 +1200

    added stuff

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,107 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="small-container">
+    <h1>EMPLOYEES</h1>
+    <employees-form @add:employee="addEmployee" />
+    <employee-table
+      :employees="employees"
+      @delete:employee="deleteEmployee"
+      @edit:employee="editEmployee"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeesForm from "./components/EmployeesForm.vue";
+import EmployeeTable from "./components/EmployeeTable.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    EmployeeTable,
+    EmployeesForm,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: "Richard Hendricks",
+          email: "richard@piedpiper.com",
+        },
+        {
+          id: 2,
+          name: "Bertram Gilfoyle",
+          email: "gilfoyle@piedpiper.com",
+        },
+        {
+          id: 3,
+          name: "Dinesh Chugtai",
+          email: "dinesh@piedpiper.com",
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.getEmployees();
+  },
+  methods: {
+    async addEmployee(employee) {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users",
+          {
+            method: "POST",
+            body: JSON.stringify(employee),
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+          }
+        );
+        const data = await response.json();
+        this.employees = [...this.employees, data];
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async deleteEmployee(id) {
+      try {
+        await fetch(`https://jsonplaceholder.typicode.com/users/${id}`, {
+          method: "DELETE",
+        });
+        this.employees = this.employees.filter(
+          (employee) => employee.id !== id
+        );
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async editEmployee(id, updated) {
+      try {
+        const response = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${id}`,
+          {
+            method: "PUT",
+            body: JSON.stringify(updated),
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+          }
+        );
+        const data = response.json();
+        this.employees.map((e) => (e.id === id ? data : e));
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async getEmployees() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
+        this.employees = data;
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+};
 </script>
 
 <style>
@@ -24,5 +112,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  background: #009435;
+  border: 1px solid #009435;
+}
+
+.small-container {
+  max-width: 680px;
 }
 </style>
---
+++ src/components/EmployeesForm.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="employees-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Employee name</label>
+      <input
+        ref="first"
+        :class="{ 'has-error': submitting && invalidName }"
+        type="text"
+        v-model="employee.name"
+        @focus="clearStatus"
+        @keypress="clearStatus"
+      />
+      <label>Employee Email</label>
+      <input
+        type="text"
+        :class="{ 'has-error': submitting && invalidEmail }"
+        v-model="employee.email"
+        @focus="clearStatus"
+      />
+
+      <p v-if="error && submitting" class="error-message">
+        ❗Please fill out all required fields
+      </p>
+      <p v-if="success" class="success-message">
+        ✅ Employee successfully added
+      </p>
+
+      <button>Add Employee</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "employees-form",
+  data() {
+    return {
+      submitting: false,
+      error: false,
+      success: false,
+      employee: {
+        name: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.submitting = true;
+      this.clearStatus();
+
+      if (this.invalidName || this.invalidEmail) {
+        this.error = true;
+        return;
+      }
+
+      this.$emit("add:employee", this.employee);
+      this.$refs.first.focus();
+      this.employee = {
+        name: "",
+        email: "",
+      };
+      this.error = false;
+      this.success = true;
+      this.submitting = false;
+    },
+    clearStatus() {
+      this.success = false;
+      this.error = false;
+    },
+  },
+  computed: {
+    invalidName() {
+      return this.employee.name === "";
+    },
+    invalidEmail() {
+      return this.employee.email === "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+#employees-form {
+  display: flex;
+}
+form {
+  width: 100%;
+  height: 100%;
+  margin-bottom: 2rem;
+}
+
+[class*="-message"] {
+  font-weight: 500;
+}
+
+.error-message {
+  color: #d33c40;
+}
+
+.success-message {
+  color: #32a95d;
+}
+</style>
